c42595832b129650674f90ae21a1e69e1e742f0a
oyetunji26 <oyetunjie5@gmail.com>
2025-08-08 23:41:03 +0100

    feat(subscription): add PATCH support to update subscription fields

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,6 +1,7 @@
 import { workflowClient } from "../config/upstash.js";
 import Subscription from "../model/subscription.model.js";
 import { SERVER_URL } from "../config/env.js";
+import dayjs from "dayjs";
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -42,6 +43,113 @@ export const getUserSubscriptions = async (req, res, next) => {
     const subscriptions = await Subscription.find({ user: req.params.id });
 
     res.status(200).json({ success: true, data: subscriptions });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteSubscription = async (req, res, next) => {
+  console.log(req.params.id);
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+
+    console.log(subscription);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+
+    if (req.user.id !== subscription.user.toString()) {
+      const error = new Error(
+        "You are not authorized to delete this subscription"
+      );
+      error.status = 403;
+      throw error;
+    }
+
+    await subscription.deleteOne();
+    res
+      .status(200)
+      .json({ success: true, message: "Subscription deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const cancelSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+
+    if (req.user.id !== subscription.user.toString()) {
+      const error = new Error(
+        "You are not authorized to cancel this subscription"
+      );
+      error.status = 403;
+      throw error;
+    }
+
+    subscription.status = "cancelled";
+    // subscription.cancelledAt = new Date();
+    await subscription.save();
+
+    res.status(200).json({ success: true, data: subscription });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUpcomingRenewal = async (req, res, next) => {
+  try {
+    const in7days = dayjs().add(7, "day").toISOString();
+    console.log(in7days);
+    const subscriptions = await Subscription.find({
+      user: req.user.id,
+      status: "active",
+      renewalDate: { $gte: dayjs(), $lte: in7days },
+    });
+    console.log(subscriptions);
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+
+    if (!subscription) {
+      const error = new Error("Subscription not found");
+      error.status = 404;
+      throw error;
+    }
+
+    if (req.user.id !== subscription.user.toString()) {
+      const error = new Error(
+        "You are not authorized to cancel this subscription"
+      );
+      error.status = 403;
+      throw error;
+    }
+
+    Object.keys(req.body).forEach((key) => {
+      subscription[key] = req.body[key];
+    });
+
+    const updateSubscription = await subscription.save();
+
+    res.status(200).json({
+      success: true,
+      data: updateSubscription,
+    });
   } catch (error) {
     next(error);
   }
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,28 +2,26 @@ import { Router } from "express";
 import { authorize } from "../middleware/auth.middleware.js";
 
 import {
+  cancelSubscription,
   createSubscription,
+  deleteSubscription,
   getUserSubscriptions,
+  getUpcomingRenewal,
+  updateSubscription,
 } from "../controllers/subscription.controller.js";
 
 const subRouter = Router();
 
 subRouter.get("/", (req, res) => res.send({ title: "GET all subscriptions" }));
-subRouter.get("/:id", (req, res) =>
-  res.send({ title: "GET subscription details" })
-);
+// subRouter.get("/:id", (req, res) =>
+//   res.send({ title: "GET subscription details" })
+// );
 subRouter.post("/", authorize, createSubscription);
-subRouter.put("/:id", (req, res) => res.send({ title: "UPDATE subscription" }));
-subRouter.delete("/:id", (req, res) =>
-  res.send({ title: "DELETE subscription" })
-);
+subRouter.patch("/:id", authorize, updateSubscription);
+subRouter.delete("/:id", authorize, deleteSubscription);
 subRouter.get("/user/:id", authorize, getUserSubscriptions);
-subRouter.put("/:id/cancel", (req, res) =>
-  res.send({ title: "CANCEL subscriptions" })
-);
+subRouter.put("/:id/cancel", authorize, cancelSubscription);
 
-subRouter.get("/upcoming-renewals", (req, res) => {
-  res.send({ title: "GET upcoming renewals" });
-});
+subRouter.get("/upcoming-renewals", authorize, getUpcomingRenewal);
 
 export default subRouter;
